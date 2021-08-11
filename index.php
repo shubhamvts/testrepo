07f15454a5e46bd216b9a6ad224a590978ce0e49
Shubham Vats <shubhamvats830@gmail.com>
2021-08-11 20:50:12 +0530

    main index of our app

---
+++ index.php
@@ -0,0 +1,14 @@
+
+
+<?php
+session_start();
+  ?>
+
+<html>
+<head> <h1> Welcome to Rock, Paper, Scissors </h1>
+   <title> Shubham Vats </title>
+</head>
+  <body>
+    <p> <a href="login.php"> Please Log In </a> </p>
+  </body>
+  </html>
